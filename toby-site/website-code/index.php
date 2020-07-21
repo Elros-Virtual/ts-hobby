3c1661f481da8a2a77734fa114f47f3ac29cf5a7
Ian Roberts <iar31.roberts@gmail.com>
2020-07-21 14:41:07 +0100

    Merging from previous repo

---
+++ toby-site/website-code/index.php
@@ -0,0 +1,10 @@
+<?php include "inc/header.php"; ?>
+<?php include "inc/nav.php"; ?>
+
+<body>
+    <div class="container">
+        <img src="img/fry.jpg" id="homescreen" alt="fry">
+    </div>
+</body>
+
+<?php include "inc/footer.php"; ?>
